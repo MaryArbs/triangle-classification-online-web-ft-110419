281daba08d19acb23ae567237c7ddd13f89978e1
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-23 18:48:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,9 +7,15 @@ class Triangle
 
   def kind()
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-      raise TriangleError
+      begin
+           raise TriangleError
+           puts error
+         end
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-      raise TriangleError
+      begin
+           raise TriangleError
+           puts error
+         end
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral

f86d739cbffdc22a40724b5f3572e07433b2baef
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-23 19:01:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,11 +16,6 @@ class Triangle
            raise TriangleError
            puts error
          end
-    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-      begin
-           raise TriangleError
-           puts error
-         end
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral

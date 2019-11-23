08c6fb16f7eb0fac21681ce69b4451a7cb90f6d2
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-23 19:04:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
         @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
         :isosceles
       else
-        (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
         :scalene
       end
     end

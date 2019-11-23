fc8e6b9aebf1b46d459238f6c812736b5c46fd70
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-23 19:07:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,9 @@ class Triangle
   end
 
   def kind()
-    if @side_1 <= 0 ||
+    if @sides.any? {|s| s == 0 } ||
+       @sides.any? {|s| s < 0 } ||
+       @side_1 <= 0 ||
        @side_2 <= 0 ||
        @side_3 <= 0||
        @side_1 + @side_2 <= @side_3 || @side_1 + @side_3 <= @side_2 || @side_2 + @side_3 <= @side_1

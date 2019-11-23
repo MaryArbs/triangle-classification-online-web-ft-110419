5e942a32cf2bfdb6ed2a40d534dab86a51a27b73
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-23 19:03:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
            puts error
          end
     elsif
-       (@side_1 == @side_2) && (@side_2 == @side_3)
+       @side_1 == @side_2 && @side_2 == @side_3
         :equilateral
       elsif
         (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
